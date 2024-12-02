2c429255b93188044b5f1446be25ed0b873f3c15
unknown <pschmidt@pasit.nl>
2024-12-02 12:14:21 +0100

    day1

--- Live/Module_1/Strafwerk/Program.cs
+++ Live/Module_1/Strafwerk/Program.cs
@@ -4,6 +4,14 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        for(int tafel = 1; tafel <= 10; tafel++)
+        {
+            Console.WriteLine($"De tafel van {tafel}");
+            for(int teller = 1; teller <= 10; teller++)
+            {
+                int result = tafel * teller;
+                Console.WriteLine($"{teller} x {tafel} = {result}");
+            }
+        }
     }
 }
