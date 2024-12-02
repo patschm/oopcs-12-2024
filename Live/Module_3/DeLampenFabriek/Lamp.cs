c7fd9a2341053c44ee802ca9d568b66eef6d593a
unknown <pschmidt@pasit.nl>
2024-12-02 16:16:55 +0100

    day1

---
+++ Live/Module_3/DeLampenFabriek/Lamp.cs
@@ -0,0 +1,74 @@
+﻿namespace DeLampenFabriek;
+
+// Dit is de blauwdruk van het object (Lamp in dit geval)
+// Dit is een Custom type
+class Lamp
+{
+    // Eigenschappen van de lamp sla ik op in fields
+    // Fields zijn veredelde variabelen.
+    // By default zijn die private
+    private int _intensiteit = 100;
+    //private ConsoleColor _kleur;
+
+    // cplusplus-iaans
+    //public void SetIntensiteit(int wdeerer)
+    //{
+    //    if (wdeerer >= 0 && wdeerer < 5000)
+    //    {
+    //        this.intensiteit = wdeerer;
+    //    }
+    //}
+    //public int GetIntensiteit()
+    //{
+    //    return this.intensiteit;
+    //}
+
+    // Een dotnetter gebruikt properties.
+    public int Intensiteit
+    {
+        get
+        {
+            return _intensiteit;
+        }
+        set
+        {
+            if (value >= 0 && value < 5000)
+            {
+                _intensiteit = value;
+            }
+        }
+    }
+    // Auto-generating property. Genereert zijn eigen private field.
+    public ConsoleColor Kleur { get; set; } = ConsoleColor.Yellow;
+
+
+    // Gedrag leg ik vast in functies of procedures.
+    // Die noemen we dan Methods
+    public void Aan()
+    {
+        Console.BackgroundColor = Kleur;
+        Console.WriteLine($"De lamp is aan en brandt met {Intensiteit} lm");
+    }
+    public void Uit()
+    {
+        Console.WriteLine("De lamp gaat uit");
+        Console.ResetColor();
+    }
+
+    // Constructors
+    // Is bedoeld om fields een initiele waarde te geven
+    // Als jij er geen defineert, genereert de compiler er een. (default constructor)
+    // Zijn niet altijd nog. Alleen als je van buitenaf een waarde EIST
+    public Lamp()
+    {
+        //Console.WriteLine("Default constructor");
+        Intensiteit = 100;
+        Kleur = ConsoleColor.Yellow;
+    }
+    public Lamp(int intensiteit, ConsoleColor kleur)
+    {
+        //SetIntensiteit(intensiteit);
+        Intensiteit = intensiteit;
+        Kleur = kleur;
+    }
+}
---
+++ Live/Module_3/DeLampenFabriek/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace DeLampenFabriek;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        // Virtuele universum. Hier leven je objecten
+        // Big bang
+
+        Lamp l1 = new Lamp();
+        //l1.intensiteit = 100;
+        l1.Kleur = ConsoleColor.Yellow;
+
+        Lamp l2 = new Lamp(300, ConsoleColor.Green);
+        //l2.intensiteit = 300;
+        //l2.kleur = ConsoleColor.Green;
+
+        l1.Aan();
+        Console.WriteLine("En verder");
+        l1.Uit();
+
+        l2.Aan();
+        Console.WriteLine("We schrijven nog meer");
+        l2.Uit();
+
+
+        Lamp x3 = new Lamp(1000, ConsoleColor.Cyan);
+        // x3.intensiteit = -1000;
+        //x3.SetIntensiteit(-1000);
+        x3.Intensiteit = -1000; 
+        Console.WriteLine(x3.Intensiteit);
+        x3.Aan();
+        Console.WriteLine("Heel helder");
+        x3.Uit();
+
+        // Property Initializers
+        Lamp x42 = new Lamp { 
+           Intensiteit = 2000, 
+            Kleur=ConsoleColor.Magenta };
+
+        x42.Aan();
+        Console.WriteLine("Superlamp");
+
+        // Big Crunch
+    }
+}
+
