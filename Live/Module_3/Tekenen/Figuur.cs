a7fdb5619ab041338af2695a4931c90dfe66be9a
unknown <pschmidt@pasit.nl>
2024-12-03 11:45:15 +0100

    day2

---
+++ Live/Module_3/Tekenen/Figuur.cs
@@ -0,0 +1,97 @@
+﻿namespace Tekenen;
+
+class Figuur
+{
+	// Fields
+	private string _soort;
+	private int _hoogte;
+	private int _breedte;
+	private float _straal;
+	private int _hoek;
+	private int _basis;
+
+	// Properties
+	public int Basis
+	{
+		get { return _basis; }
+		set 
+		{ 
+			if (value >= 0) _basis = value; 
+		}
+	}
+	public int Hoek
+	{
+		get { return _hoek; }
+		set 
+		{ 
+			if (value >= 0)_hoek = value; 
+		}
+	}
+	public float Straal
+	{
+		get { return _straal; }
+		set 
+		{ 
+			if (value >= 0) _straal = value; 
+		}
+	}
+	public int Breedte
+	{
+		get { return _breedte; }
+		set 
+		{
+			if (value >= 0)_breedte = value; 
+		}
+	}
+	public int Hoogte
+	{
+		get { return _hoogte; }
+		set 
+		{
+			if (value >=0) _hoogte = value; 
+		}
+	}
+    public ConsoleColor Kleur { get; set; } = ConsoleColor.Black;
+	public double Oppervlakte
+	{
+		get
+		{
+			switch(_soort)
+			{
+				case "cirkel":
+					return Math.PI * Math.Pow(Straal, 2);
+				case "driehoek":
+					return .5 * Basis * Hoogte;
+				case "rechthoek":
+					return Hoogte * Breedte;
+				default:
+					return 0;
+			}
+		}
+	}
+
+	// Methods (Gedrag)
+	public void Teken()
+	{
+		Console.ForegroundColor = Kleur;
+
+        switch (_soort)
+        {
+            case "cirkel":
+                Console.WriteLine($"Cirkel met straal {Straal}");
+				break;
+            case "driehoek":
+                Console.WriteLine($"Driehoek met basis: {Basis} en hoogte {Hoogte}");
+                break;
+            case "rechthoek":
+                Console.WriteLine($"Rechthoek met hoogte {Hoogte} en breedte {Breedte}");
+				break;
+        }
+    }
+
+	// Constructor
+    public Figuur(string soort)
+    {
+        _soort = soort;
+    }
+}
--- Live/Module_3/Tekenen/Program.cs
+++ Live/Module_3/Tekenen/Program.cs
@@ -4,6 +4,32 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Figuur c1 = new Figuur("cirkel")
+        {
+            Straal = 100,
+            Kleur = ConsoleColor.DarkBlue
+        };
+
+        Figuur r1 = new Figuur("rechthoek")
+        {
+            Hoogte = 100,
+            Breedte = 200,
+            Kleur = ConsoleColor.Green
+        };
+
+        Figuur d1 = new Figuur("driehoek")
+        {
+            Hoogte = 100,
+            Basis = 200,
+            Hoek = 60
+        };
+
+        Console.WriteLine($"Oppervlakte cirkel = {c1.Oppervlakte}");
+        Console.WriteLine($"Oppervlakte rechthoek = {r1.Oppervlakte}");
+        Console.WriteLine($"Oppervlakte driehoek = {d1.Oppervlakte}");
+
+        c1.Teken();
+        r1.Teken();
+        d1.Teken();
     }
 }
